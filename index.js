ad7e5597ba669ace8398bd4e77bf79d44f880b6f
Michael-Bryant Choa <michael-bryant.choa@hautelook.com>
2017-04-04 23:14:29 -0700

    Change port

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/', function (req, res) {
 });
 
 // start server
-var server = app.listen(3000, function () {
+var server = app.listen(4000, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
