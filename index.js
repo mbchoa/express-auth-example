4f7d089ff576bd7a9f0774e3a8a6a7427b0fbf9f
Michael-Bryant Choa <michael-bryant.choa@hautelook.com>
2017-04-04 19:25:10 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var session = require('express-session');
+
+var FileStore = require('session-file-store')(session);
+var app = require('express')();
+
+// setup middleware
+app.use(require('morgan')('dev'));
+app.use(session({
+    name: 'server-session-cookie-id',
+    secret: 'my secret',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+}));
+
+app.use(function printSession (req, res, next) {
+    console.log('req.session', req.session);
+    next();
+});
+
+// setup routes
+app.get('/', function initViewsCount (req, res, next) {
+    if (typeof req.session.views === 'undefined') {
+        req.session.views = 1;
+        return res.send('Welcome to the files session demo.  Refresh page!');
+    }
+    next();
+});
+
+app.get('/', function incrementViewsCount (req, res, next) {
+    req.session.views++;
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>views: ' + req.session.views + '</p>\n');
+    res.send();
+});
+
+// start server
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
